27fa317f9e705e6a5140a1e0b42a63c2c4202482
tuanh24801 <tuanh12a12001@gmail.com>
2021-09-19 09:16:19 +0700

    Home Work CSE

---
+++ Restaurant_Ex/Food_Ex/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="header-inner">
+                <div class="logo">
+                    <a href="#"><img src="./Image/logo.png" alt=""></a>
+                </div>
+                <div class="menu">
+                    <ul>
+                        <a href="#"><li>Home</li></a>
+                        <a href="#"><li>Categories</li></a>
+                        <a href="#"><li>Foods</li></a>
+                        <a href="#"><li>Contact</li></a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="SearchFood">
+            <div class="container">
+                <form action="">
+                    <input type="Search" name = "Search" placeholder="Search For Food...">
+                    <input type="Submit" name = "Sba" class="btn">
+                </form>
+            </div>
+        </div>
+        <div class="body">
+            <h1>Food Menu</h1>
+            <div class="content">
+                <div class="row">
+                    <div class="menu_box">
+                        <div class="img_menu">
+                            <img src="./Image/menu-pizza.jpg" alt="">
+                        </div>
+                        <div class="title_menu">
+                            <h2>Food Title</h2>
+                            <h2>$2.3</h2>
+                            <p>Made with italian Sauce. Chicken, and organice vegetables</p>
+                            <p class="ordernow">Order now</p>
+                        </div>
+                    </div>
+                    <div class="menu_box">
+                        <div class="img_menu">
+                            <img src="./Image/menu-momo.jpg" alt="">
+                        </div>
+                        <div class="title_menu">
+                            <h2>Food Title</h2>
+                            <h2>$2.3</h2>
+                            <p>Made with italian Sauce. Chicken, and organice vegetables</p>
+                            <p class="ordernow">Order now</p>
+                        </div>
+                    </div> 
+                </div>
+                <div class="row">
+                    <div class="menu_box">
+                        <div class="img_menu">
+                            <img src="./Image/menu-burger.jpg" alt="">
+                        </div>
+                        <div class="title_menu">
+                            <h2>Food Title</h2>
+                            <h2>$2.3</h2>
+                            <p>Made with italian Sauce. Chicken, and organice vegetables</p>
+                            <p class="ordernow">Order now</p>
+                        </div>
+                    </div>
+                    <div class="menu_box">
+                        <div class="img_menu">
+                            <img src="./Image/menu-pizza.jpg" alt="">
+                        </div>
+                        <div class="title_menu">
+                            <h2>Food Title</h2>
+                            <h2>$2.3</h2>
+                            <p>Made with italian Sauce. Chicken, and organice vegetables</p>
+                            <p class="ordernow">Order now</p>
+                        </div>
+                    </div> 
+                </div>
+                <div class="row">
+                    <div class="menu_box">
+                        <div class="img_menu">
+                            <img src="./Image/menu-pizza.jpg" alt="">
+                        </div>
+                        <div class="title_menu">
+                            <h2>Food Title</h2>
+                            <h2>$2.3</h2>
+                            <p>Made with italian Sauce. Chicken, and organice vegetables</p>
+                            <p class="ordernow">Order now</p>
+                        </div>
+                    </div>
+                    <div class="menu_box">
+                        <div class="img_menu">
+                            <img src="./Image/menu-pizza.jpg" alt="">
+                        </div>
+                        <div class="title_menu">
+                            <h2>Food Title</h2>
+                            <h2>$2.3</h2>
+                            <p>Made with italian Sauce. Chicken, and organice vegetables</p>
+                            <p class="ordernow">Order now</p>
+                        </div>
+                    </div> 
+                </div>   
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="logo_information">
+                   <ul>
+                       <a href="#"><li><img src="https://img.icons8.com/color/48/000000/facebook-new.png"/></li></a>
+                       <a href="#"><li><img src="https://img.icons8.com/fluency/48/000000/instagram-new.png"/></li></a>
+                       <a href="#"><li><img src="https://img.icons8.com/color/48/000000/twitter--v1.png"/></li></a>
+                   </ul>
+            </div>
+            <div class="Text_information">
+                    <p>All right reserved. Designed By <a href="#"> CSE TLU</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Restaurant_Ex/Order_Ex_1/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="header-inner">
+                <div class="logo">
+                    <a href="#"><img src="./Image/logo.png" alt=""></a>
+                </div>
+                <div class="menu">
+                    <ul>
+                        <a href="#"><li>Home</li></a>
+                        <a href="#"><li>Categories</li></a>
+                        <a href="#"><li>Foods</li></a>
+                        <a href="#"><li>Contact</li></a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <section class="food-search">
+            <div class="container">
+            
+            <h2 class="text-center text-white">Fill this form to confirm your order.</h2>
+
+            <form action="#" class="order">
+                <fieldset>
+                    <legend>Selected Food</legend>
+
+                    <div class="food-menu-img">
+                        <img src="Image/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                    </div>
+    
+                    <div class="food-menu-desc">
+                        <h3>Food Title</h3>
+                        <p class="food-price">$2.3</p>
+
+                        <div class="order-label">Quantity</div>
+                        <input type="number" name="qty" class="input-responsive" value="1" required>
+                        
+                    </div>
+
+                </fieldset>
+                
+                <fieldset>
+                    <legend>Delivery Details</legend>
+                    <div class="order-label">Full Name</div>
+                    <input type="text" name="full-name" placeholder="E.g. Vijay Thapa" class="input-responsive" required>
+
+                    <div class="order-label">Phone Number</div>
+                    <input type="tel" name="contact" placeholder="E.g. 9843xxxxxx" class="input-responsive" required>
+
+                    <div class="order-label">Email</div>
+                    <input type="email" name="email" placeholder="E.g. hi@vijaythapa.com" class="input-responsive" required>
+
+                    <div class="order-label">Address</div>
+                    <textarea name="address" rows="10" placeholder="E.g. Street, City, Country" class="input-responsive" required></textarea>
+
+                    <input type="submit" name="submit" value="Confirm Order" class="btn btn-primary">
+                </fieldset>
+
+            </form>
+
+        </div>
+    </section>
+        <div class="bottom">
+            <div class="logo_information">
+                   <ul>
+                       <a href="#"><li><img src="https://img.icons8.com/color/48/000000/facebook-new.png"/></li></a>
+                       <a href="#"><li><img src="https://img.icons8.com/fluency/48/000000/instagram-new.png"/></li></a>
+                       <a href="#"><li><img src="https://img.icons8.com/color/48/000000/twitter--v1.png"/></li></a>
+                   </ul>
+            </div>
+            <div class="Text_information">
+                    <p>All right reserved. Designed By <a href="#"> CSE TLU</a></p>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
